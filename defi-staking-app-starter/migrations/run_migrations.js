9cfc45cddca1f4c0c3a05b6f661f5857316d8588
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-23 20:03:09 +0100

    deploy update

--- defi-staking-app-starter/migrations/run_migrations.js
+++ defi-staking-app-starter/migrations/run_migrations.js
@@ -4,5 +4,5 @@ const Migrations = artifacts.require('Migrations');
 //how to make updates to smart contract by re deploying etc!!
 
 module.exports = function deployer() {
-    deployer.deploy(Migrations)
+    deployer.deploy(Migrations);
 }
