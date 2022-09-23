dabf8596e5c9a19f89f7d309e1b953787f6692e5
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-23 20:02:31 +0100

    deploy tether

---
+++ defi-staking-app-starter/migrations/deploy_contracts.js
@@ -0,0 +1,8 @@
+const Tether = artifacts.require('Tether');
+
+
+//how to make updates to smart contract by re deploying etc!!
+
+module.exports = async function deployer() {
+    await deployer.deploy(Tether);
+}
--- defi-staking-app-starter/migrations/run_migrations.js
+++ defi-staking-app-starter/migrations/run_migrations.js
@@ -3,7 +3,6 @@ const Migrations = artifacts.require('Migrations');
 
 //how to make updates to smart contract by re deploying etc!!
 
-
 module.exports = function deployer() {
-    deployer.deplo(Migrations)
+    deployer.deploy(Migrations)
 }
