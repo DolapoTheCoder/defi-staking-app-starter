91476667c462ef193694aea909c2964d8830e45a
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-09-23 19:53:21 +0100

    Migrate contracts

---
+++ defi-staking-app-starter/migrations/run_migrations.js
@@ -0,0 +1,9 @@
+const Migrations = artifacts.require('Migrations');
+
+
+//how to make updates to smart contract by re deploying etc!!
+
+
+module.exports = function deployer() {
+    deployer.deplo(Migrations)
+}
